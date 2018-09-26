2325a872267e026daa214ef8a5d75adc2d15db36
Burach0k <Burak_andre@mail.ru>
2018-09-26 23:50:06 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue + " ";
   }
 
   add(number) {
-    // your implementation
+    this.initialValue = this.initialValue +"+"+ number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.initialValue =this.initialValue +"-"+ number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.initialValue = this.initialValue +"*"+ number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.initialValue = "("+this.initialValue +"/"+ number+")";
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    var i = 0;
+    var j = 0;
+    var a = "";
+    var c = "";
+    var r = ")";
+    var b;
+    for(i = 0; i<20; i++){
+      b =this.initialValue[this.initialValue.length-1 - i];
+      if((b == "+")||(b == "-")||(b == "*")||(b == "/")||(b == undefined)||(b == ",")){
+        break;
+      }
+      if(b != ")"){
+        a = this.initialValue[this.initialValue.length - 1 - i] +a;
+        
+      }else{
+        c = c + r;
+      }
+        j++;
+    }
+    this.initialValue = this.initialValue.substring(0,this.initialValue.length-j) + 'Math.pow(' + a + ', ' + number + ') ' + c;
+    return this;
   }
+  toString(){
+    console.log(this.initialValue);
+    return eval(this.initialValue);
+  }
+
 }
 
 module.exports = SmartCalculator;
